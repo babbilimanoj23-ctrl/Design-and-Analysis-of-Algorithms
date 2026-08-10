7f8eb41e862406771616dc1cb0854f710de1dec7
Babbili Manoj <babbilimanoj23@gmail.com>
2026-08-10 08:44:02 +0530

    Refactor search functions and improve user input handling
    Refactored linear search function and added binary search implementation. Enhanced main function for user interaction.

--- Linearsearch.py
+++ Linearsearch.py
@@ -1,12 +1,55 @@
-def linear_search (arr,key):
+def linear_search(arr, key):
     for i in range(len(arr)):
         if arr[i] == key:
-            print("Element found at position",i+1)
             return i
-        print("Element not found")
+    return -1
 
-        arr = [10,20,30,40]
-        key = 20
-        linear_search(arr,key)
+def binary_search(arr, key):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
 
 
+
+def main():
+    n = int(input("Enter number of elements: "))
+
+    arr = list(map(int, input("Enter elements:\n").split()))
+
+    key = int(input("Enter element to search: "))
+
+    print("\nSearch Algorithms")
+    print("1. Linear Search")
+    print("2. Binary Search")
+
+    choice = int(input("\nEnter your choice: "))
+
+    if choice == 1:
+        pos = linear_search(arr, key)
+
+    elif choice == 2:
+        pos = binary_search(arr, key)
+
+    else:
+        print("Invalid Choice")
+        return
+
+    if pos == -1:
+        print("\nElement Not Found.")
+    else:
+        print(f"\nElement Found at Position {pos + 1}")
+
+
+if __name__ == "__main__":
+    main()
